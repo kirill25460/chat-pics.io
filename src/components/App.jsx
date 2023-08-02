ab4ace2ea7308d297c7d426e81eeadd283ae948c
Kyrylo Dyachenko <104459315+kirill25460@users.noreply.github.com>
2023-08-02 20:52:53 +0300

    try

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import example from '../example.json';
 import { GlobalStyle } from './GlobalStyles';
+// import { leftIMG } from './GlobalStyles';
 import { MessageArea } from './MessageArea/MessageArea';
+// import left from '../images/lev.png'
 // console.log(example.username)
   example.comments.map(ex => console.log(ex.user))
 
 export const App = () => {
   return (
     <GlobalStyle>
-       
+
+      
       {example.comments.map(ex =>
        <MessageArea 
        postId={ex.postId}
--- src/components/GlobalStyles.jsx
+++ src/components/GlobalStyles.jsx
@@ -1,5 +1,14 @@
 import styles from 'styled-components';
 
+
 export const GlobalStyle = styles.div`
+// backgroundImage: "url(.../public/prav.png)";
+// backgroundRepeat: 'no-repeat';
+// backgroundSize: 'cover';
 background-color: #0080ff;
+`
+
+export const leftIMG = styles.img`
+// width:75px;
+// height:50px;
 `
